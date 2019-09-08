50154bd4651c6ca5c1fa2995557c89fc7fdf7124
Saurabh <j.saur95@gmail.com>
2019-09-08 07:45:16 -0400

    npm test and build
    Done with tests, multi-step builds and nginx servin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Saurabh! How are you? I am good.
+          Hi Saurabh! How are you? I am good!
         </p>
 
         <a
